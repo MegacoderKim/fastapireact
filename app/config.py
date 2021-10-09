a633599083a5b2429574c09f6a02082b6b3b3af3
Harrison Kimani <harriskimm@outlook.com>
2021-10-09 14:00:13 +0300

    Add migrations aerich

--- app/config.py
+++ app/config.py
@@ -2,14 +2,15 @@ import os
 import logging
 
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 logger = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    environment: str = os.getenv("ENVIRONMNET", "dev")
-    testing: int = os.getenv("TESTING", 0)
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 
 @lru_cache
