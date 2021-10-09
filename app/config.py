d1e8493ddac4c873f95a583fc936555d41a36aa9
Harrison Kimani <harriskimm@outlook.com>
2021-10-09 08:24:30 +0300

    configure project start

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from functools import lru_cache
+from pydantic import BaseSettings
+
+logger = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMNET", "dev")
+    testing: int = os.getenv("TESTING", 0)
+
+
+@lru_cache
+def get_settings() -> BaseSettings:
+    logger.info("Loading applications configuration...")
+    return Settings()
