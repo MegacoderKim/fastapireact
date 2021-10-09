d5a91e2577f0620308c385f1bd0674da154cf750
Harrison Kimani <harriskimm@outlook.com>
2021-10-09 08:45:54 +0300

    Add read me file with docker run instructions

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import logging
+import os
+
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
 
 app = FastAPI()
-
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
